ceff820477b11364cb64441574d480bd59b1b22d
vigneshxprabhu <vigneshxprabhu@gmail.com>
2026-06-29 22:39:39 +0530

    authservice

---
+++ src/main/java/com/example/playlistify/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.example.playlistify.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+        public String getAccessToken(){
+            return "your acess token";
+        }
+}
--- src/main/java/com/example/playlistify/service/SpotifyService.java
+++ src/main/java/com/example/playlistify/service/SpotifyService.java
@@ -1,22 +1,21 @@
 package com.example.playlistify.service;
 
-import com.example.playlistify.model.Track;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class SpotifyService {
 
-   public List<Track> getLikedSongs(){
-     List<Track> songs=new ArrayList<>();
+    private final AuthService authService;
 
-     Track song1=new Track();
+    public SpotifyService(AuthService authService) {
+        this.authService = authService;
+    }
 
-     song1.setId("2");
-     song1.setName("do u love me");
-     songs.add(song1);
-     return songs;
-   }
+    public String testSpotifyService() {
+
+        String token = authService.getAccessToken();
+
+        return token;
+    }
+
 }
