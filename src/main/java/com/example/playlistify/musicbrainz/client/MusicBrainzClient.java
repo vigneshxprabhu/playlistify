b96c9faa96c24b057ad2a43f3b832fd39ed5b33c
vigneshxprabhu <vigneshxprabhu@gmail.com>
2026-09-10 20:35:03 +0530

    Integrate MusicBrainz artist lookup

--- src/main/java/com/example/playlistify/musicbrainz/client/MusicBrainzClient.java
+++ src/main/java/com/example/playlistify/musicbrainz/client/MusicBrainzClient.java
@@ -5,7 +5,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import com.example.playlistify.musicbrainz.dto.response.MusicBrainzArtistResponse;
 import com.example.playlistify.musicbrainz.dto.response.MusicBrainzArtistSearchResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MusicBrainzClient {
 
@@ -30,11 +32,47 @@ public class MusicBrainzClient {
                     return null;
 
     }
+    public MusicBrainzArtistResponse getArtist(String mbid) throws Exception {
+
+        String url ="https://musicbrainz.org/ws/2/artist/"
+        +mbid
+        +"?inc=genres&fmt=json";
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create(url))
+        .header("User-Agent", "Playlistify/1.0")
+        .GET()
+        .build();
+
+
+        HttpResponse <String> response=client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        MusicBrainzArtistResponse artistResponse =
+        objectMapper.readValue(
+                response.body(),
+                MusicBrainzArtistResponse.class
+        );
+        return artistResponse;
+
+    
+
+    }
 
     public static void main(String[] args) throws Exception {
 
     MusicBrainzClient client = new MusicBrainzClient();
 
     client.searchArtist("Metallica");
+    MusicBrainzArtistResponse artist =
+            client.getArtist("65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab");
+
+    System.out.println("Artist: " + artist.getName());
+    System.out.println("Genres: " + artist.getGenres());
     }
 }
--- src/main/java/com/example/playlistify/musicbrainz/service/MusicBrainzProvider.java
+++ src/main/java/com/example/playlistify/musicbrainz/service/MusicBrainzProvider.java
@@ -1,15 +1,28 @@
 package com.example.playlistify.musicbrainz.service;
-
 import com.example.playlistify.model.ArtistDetails;
+import com.example.playlistify.musicbrainz.client.MusicBrainzClient;
+import com.example.playlistify.musicbrainz.dto.response.MusicBrainzArtistSearchResponse;
 import com.example.playlistify.service.ArtistMetadataProvider;
 
 public class MusicBrainzProvider implements ArtistMetadataProvider{
 
+    private final MusicBrainzClient musicBrainzClient;
+    public MusicBrainzProvider(MusicBrainzClient musicBrainzClient) {
+    this.musicBrainzClient = musicBrainzClient;
+}
+
     @Override
     public ArtistDetails fetchArtistDetails (String artistName)throws Exception{
-        return null;
+    
+           MusicBrainzArtistSearchResponse searchResponse =musicBrainzClient.searchArtist(artistName);
 
-        
+            String mbid = searchResponse.getArtists().get(0).getId();
+
+             System.out.println("Artist: " + artistName);
+            System.out.println("MBID: " + mbid);
+
+
+        return null;
 
     }
 }
